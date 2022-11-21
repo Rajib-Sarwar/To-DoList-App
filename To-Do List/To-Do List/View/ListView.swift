84c214c7a93e627dd6a0bb7733890f78b6bad928
Ayon Chowdhury <md.rajib.sarwar@gmail.com>
2022-11-21 08:43:58 -0500

    improved user experience and added animations

--- To-Do List/To-Do List/View/ListView.swift
+++ To-Do List/To-Do List/View/ListView.swift
@@ -11,19 +11,26 @@ struct ListView: View {
 
     @EnvironmentObject var listViewModel: ListViewModel
     var body: some View {
-        List {
-            ForEach(listViewModel.items) { item in
-                ListRowView(item: item)
-                    .onTapGesture {
-                        withAnimation(.linear) {
-                            listViewModel.updateItem(item: item)
-                        }
+        ZStack {
+            if listViewModel.items.isEmpty {
+                NoItemsView()
+                    .transition(AnyTransition.opacity.animation(.easeIn))
+            } else {
+                List {
+                    ForEach(listViewModel.items) { item in
+                        ListRowView(item: item)
+                            .onTapGesture {
+                                withAnimation(.linear) {
+                                    listViewModel.updateItem(item: item)
+                                }
+                            }
                     }
+                    .onDelete(perform: listViewModel.deleteItem)
+                    .onMove(perform: listViewModel.moveItem)
+                }
+                .listStyle(PlainListStyle())
             }
-            .onDelete(perform: listViewModel.deleteItem)
-            .onMove(perform: listViewModel.moveItem)
         }
-        .listStyle(PlainListStyle())
         .navigationTitle("To-Do List 📝")
         .navigationBarItems(leading: EditButton(), trailing:
             NavigationLink("Add", destination: AddView())
@@ -32,7 +39,6 @@ struct ListView: View {
 }
 
 struct ListView_Previews: PreviewProvider {
-    //1.
     static var previews: some View {
         NavigationView {
             ListView()
--- To-Do List/To-Do List/View/NoItemsView.swift
+++ To-Do List/To-Do List/View/NoItemsView.swift
@@ -8,8 +8,63 @@
 import SwiftUI
 
 struct NoItemsView: View {
+    
+    //1.
+    @State var animate: Bool = false
+    //6.1
+    let accent = Color("AccentColor")
+    let secondaryAccent = Color("SecondaryAccentColor")
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(spacing: 10) {
+                Text("There are no items")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("Are you a productive person? I think you should click the add a bunch of items to your to do list! Hit the add button to do so")
+                    .padding(.bottom, 20)
+                NavigationLink(destination: AddView()) {
+                    Text("Add Something 🥳")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                    //6.2
+                        .background(animate ? secondaryAccent : accent)
+                        .cornerRadius(10)
+                }
+                //6.3
+                .padding(.horizontal, animate ? 30 : 50)
+                .shadow(color: animate ? secondaryAccent.opacity(0.7) : accent.opacity(0.7),
+                        radius: animate ? 30 : 10,
+                        x: 0.0,
+                        y: animate ? 30 : 50)
+                .scaleEffect(animate ? 1.1 : 1.0)
+                .offset(y: animate ? -7 : 0)
+            }
+            .multilineTextAlignment(.center)
+            .padding(40)
+            //2.
+            .onAppear(perform: addAnimation)
+        }
+        .frame(maxWidth: .infinity,
+               maxHeight: .infinity)
+    }
+    
+    func addAnimation() {
+        //3.
+        guard !animate else { return }
+        //4.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            //5.
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 2.0)
+                    .repeatForever()
+            ) {
+                animate.toggle()
+            }
+        }
     }
 }
 
